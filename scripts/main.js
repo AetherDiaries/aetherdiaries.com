3874684c43719d3880fc7fcf604acf52d1aa0073
AetherDiaries <aetherdiariesyt@gmail.com>
2024-11-24 14:20:21 +0530

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -4,16 +4,19 @@ const toggleFadeElements = document.querySelectorAll('.toggle-fade');
 const navPriceButton = document.getElementById('nav-price-chart');
 const defaultPanelStyle = pricingChart.style;
 const shiftValue = "30px";
+const pricingChartNewStyles = {
+    transform: "translateY(-15vh)",
+    borderRadius: "13px",
+    backgroundColor: "rgba(0, 0, 0, 0.4)"
+};
 
 navPriceButton.addEventListener('click', () => {
     pricingChartButton.click();
 });
 
-pricingChartButton.addEventListener('change', () => {
+function pricingDetailsReveal() {
     if (pricingChartButton.checked) {
-        pricingChart.style.transform = "translateY(-15vh)";
-        pricingChart.style.borderRadius = "13px";
-        pricingChart.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+        Object.assign(pricingChart.style, pricingChartNewStyles);
         // Fade out
         toggleFadeElements.forEach(toggleFadeElement => {
             toggleFadeElement.style.opacity = 0;
@@ -32,4 +35,24 @@ pricingChartButton.addEventListener('change', () => {
             toggleFadeElement.style.transform = `translateX(0)`;
         });
     }
-});
+}
+
+pricingChartButton.addEventListener('change', pricingDetailsReveal);
+const pricingChartDetails = pricingDetails.innerHTML;
+
+function handleResize() {
+    const pricingDetails = document.getElementById("pricingDetails");
+    
+    if (window.innerWidth < 1400) {
+        pricingDetails.innerHTML = `
+        <div class="mobile-message">
+        <span>The pricing chart is unavailable for small screens. Please view on a larger device.</span>
+        </div>
+        `;
+    } else {
+        pricingDetails.innerHTML = pricingChartDetails;
+    }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
