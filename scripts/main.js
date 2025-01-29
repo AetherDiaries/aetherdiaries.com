1877f6fb867b9072e205232ef07ffe0ff4ea3da9
AetherDiaries <aetherdiariesyt@gmail.com>
2025-01-30 03:23:48 +0530

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -10,9 +10,9 @@ const pricingChartNewStyles = {
     backgroundColor: "rgba(0, 0, 0, 0.4)"
 };
 
-navPriceButton.addEventListener('click', () => {
-    pricingChartButton.click();
-});
+navPriceButton.addEventListener('click', () => pricingChartButton.click());
+
+document.getElementById('closePricingDetails'). onclick = pricingChart.click();
 
 function pricingDetailsReveal() {
     const pricingChartChild = pricingChart.firstElementChild;
@@ -57,3 +57,23 @@ function handleResize() {
 
 handleResize();
 window.addEventListener("resize", handleResize);
+
+const toggleOverlay = (hovered, target, source="", text = "") => {
+    target.style.border = hovered ? "none" : "";
+    const imgSample = target.querySelector(".overlaySample");
+    target.classList.toggle("dimmed", hovered);
+    
+    imgSample.innerHTML = hovered ? `Sample<div style = "display: block; font-size: 14px;">${text}</div>` : "";
+    imgSample.style.width = hovered ? "100%" : "0";
+    imgSample.style.height = hovered ? "100%" : "0";
+    imgSample.style.transform = hovered ? source==="p1" ? "rotateY(-15deg)" : "rotateY(15deg)" : "";
+    imgSample.style.opacity = hovered ? "1" : "0";
+};
+
+const panel1 = document.getElementById("panel1");
+const panel2 = document.getElementById("panel2");
+
+panel1.addEventListener("mouseenter", () => toggleOverlay(true, panel2, "p1", "(Standard)"));
+panel1.addEventListener("mouseleave", () => toggleOverlay(false, panel2));
+panel2.addEventListener("mouseenter", () => toggleOverlay(true, panel1, "p2", "(Custom)"));
+panel2.addEventListener("mouseleave", () => toggleOverlay(false, panel1));
